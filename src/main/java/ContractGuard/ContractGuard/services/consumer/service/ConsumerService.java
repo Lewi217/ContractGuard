1cc67c86eed799748225d734a8e787f936cf715c
wanjohi lewi <wanjohilewi7@gmail.com>
2025-12-12 23:15:53 +0300

    code clean up

--- src/main/java/ContractGuard/ContractGuard/services/consumer/service/ConsumerService.java
+++ src/main/java/ContractGuard/ContractGuard/services/consumer/service/ConsumerService.java
@@ -1,4 +1,21 @@
 package ContractGuard.ContractGuard.services.consumer.service;
 
-public class ConsumerService {
+import ContractGuard.ContractGuard.services.consumer.dto.ConsumerResponse;
+import ContractGuard.ContractGuard.services.consumer.dto.CreateConsumerRequest;
+import ContractGuard.ContractGuard.services.consumer.dto.UpdateConsumerRequest;
+import ContractGuard.ContractGuard.services.consumer.model.Consumer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ConsumerService {
+    ConsumerResponse registerConsumer(CreateConsumerRequest request);
+    ConsumerResponse getConsumer(UUID consumerId);
+    List<ConsumerResponse> getConsumersByOrganization(UUID organizationId);
+    List<ConsumerResponse> getActiveConsumers(UUID organizationId);
+    List<ConsumerResponse> searchConsumersByName(UUID organizationId, String searchTerm);
+    Consumer getConsumerByApiKey(String apiKey);
+    ConsumerResponse updateConsumer(UUID consumerId, UpdateConsumerRequest request);
+    ConsumerResponse deactivateConsumer(UUID consumerId);
+    void deleteConsumer(UUID consumerId);
 }
